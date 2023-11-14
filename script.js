3c79a4d74273e0687015ae183d788f75d1385fcf
CyrilMougin <c.mougin@hotmail.fr>
2023-11-14 22:26:07 +0100

    First JS function
    Add the following functions :
    getPlayerChoice
    getComputerChoice
    
    Map the button-choose to the addEventListener of the player choice

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 console.log("ahahah")
+
+
+
+
+
+const chooseButton = document.getElementById("button-choose");
+
+
+chooseButton.addEventListener("click", () =>{
+    let choice ;
+    do {
+        choice = prompt("rock Paper or scissor") ;
+    } while (!["PAPER","ROCK","SCISSOR"].includes(choice.toUpperCase()))
+    return choice ;
+});
+
+
+
+function getPlayerChoice() {
+    let choice = prompt("rock Paper or scissor");
+    return choice
+}
+
+function getComputerChoice() {
+    let choice = ["rock", "scissor", "paper"];
+    return choice[(Math.floor(Math.random() * choice.length))];
+}
