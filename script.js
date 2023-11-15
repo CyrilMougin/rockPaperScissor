7f5f19d619cbe8c3e72ed1a11e4a25dc1ba273c4
CyrilMougin <c.mougin@hotmail.fr>
2023-11-15 21:22:05 +0100

    finished project
    project finished with some tweaks and added game function

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+let GAMEROUNDCOUNTER = 0;
+let PLAYERSCORE = 0;
+let COMPUTERSCORE =0;
+
 
 const chooseButton = document.getElementById("button-choose");
 
 
-console.log( playRound("ROCK","scissor"));
 
 
 chooseButton.addEventListener("click", () =>{
@@ -10,7 +13,7 @@ chooseButton.addEventListener("click", () =>{
     do {
         choice = prompt("rock Paper or scissor") ;
     } while (!["PAPER","ROCK","SCISSOR"].includes(choice.toUpperCase()))
-    return choice ;
+    return playRound(choice, getComputerChoice()) ;
 });
 
 function getComputerChoice() {
@@ -19,17 +22,59 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+    let winnerBool;
     playerSelection = playerSelection.toUpperCase();
     computerSelection = computerSelection.toUpperCase();
     let youWinMessage = `you win this round ! ${playerSelection} beat ${computerSelection} !`;
     let youLooseMessage = `you lost the round ! ${computerSelection} beat ${playerSelection} !`;
-    
+
+    if (playerSelection == computerSelection) {
+        console.log("it's a tie")
+        return 0;
+    }
     if ( playerSelection == "ROCK"    && computerSelection == "SCISSOR" ||
          playerSelection == "PAPER"   && computerSelection == "ROCK"    ||
-         playerSelection == "SCISSOR" && computerSelection == "PAPER") { return youWinMessage}
-    else return youLooseMessage;
+         playerSelection == "SCISSOR" && computerSelection == "PAPER") {
+            winnerBool = true;
+            console.log(youWinMessage);
+            return game(winnerBool);
+        }
+    else {
+        winnerBool = false;
+        console.log(youLooseMessage);
+        return game(winnerBool);
+    } 
 }
-         
+
+
+function game(winnerBool) {
+    
+    if (winnerBool == true) {
+        
+        PLAYERSCORE++;
+    } 
+    if (winnerBool == false) {
+        COMPUTERSCORE++;
+    } 
+    GAMEROUNDCOUNTER++;
+
+    console.log(`player :${PLAYERSCORE}, computer :${COMPUTERSCORE}`)
+
+    if ( GAMEROUNDCOUNTER == 5 && PLAYERSCORE > COMPUTERSCORE ) {
+        GAMEROUNDCOUNTER = 0;
+        COMPUTERSCORE = 0;
+        PLAYERSCORE = 0;
+        console.log("you win !!!!!")
+    } 
+    if ( GAMEROUNDCOUNTER == 5 && COMPUTERSCORE > PLAYERSCORE) {
+        GAMEROUNDCOUNTER = 0;
+        COMPUTERSCORE = 0;
+        PLAYERSCORE= 0;
+        console.log("Oh no you lose")
+    } 
+
+    return 0;
+}
     
        
     
